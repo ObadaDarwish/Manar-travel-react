d3bd4d692b03a0e52f9240db08475df2a4874553
Obada Darwish <obada_567@hotmail.co.uk>
2021-02-04 00:56:22 +0200

    add app.yaml

--- src/app.settings.js
+++ src/app.settings.js
@@ -1,14 +1,12 @@
 export const get_api_end_point = () => {
-    let endPoint = 'http://localhost:3000';
-    switch (process.env.REACT_APP_CUSTOM_ENV) {
-        case 'development':
-            endPoint = 'http://localhost:3001';
-            break;
+    let endPoint = 'http://localhost:3001';
+    console.log(process.env.NODE_ENV)
+    switch (process.env.NODE_ENV) {
         case 'production':
-            endPoint = 'http://localhost:3001';
+            endPoint = 'http://localhost:3002';
             break;
         default:
-            endPoint = 'envdsasda';
+            endPoint = 'http://localhost:3001';
     }
     return endPoint;
 };
